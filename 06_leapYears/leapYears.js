534808d744bea17a5b07f23d15b79aecd6cc5dc1
richtext <107862568+RchText@users.noreply.github.com>
2022-12-03 23:50:48 +0100

    Complete ex6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    result = false;
+
+    if (year % 4 == 0) {
+        if (year % 100 == 0 && year % 400 == 0) {
+            result = true;
+        }
+        else if (year % 100 == 0 && year % 400 != 0) {
+            result = false;
+        }
+        else {
+            result = true;
+        }
+    }
+
+    return result;
 
 };
 
