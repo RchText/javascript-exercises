7d23b96b3295427405d99db415f06b0b0c348eab
richtext <107862568+RchText@users.noreply.github.com>
2022-12-04 00:34:20 +0100

    Complete Ex. 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,63 @@
-const add = function() {
+const add = function(num1, num2) {
+  result = num1 + num2;
+  return result;
 	
 };
 
-const subtract = function() {
+const subtract = function(num1, num2) {
+	result = num1 - num2;
+  return result;
+};
+
+const sum = function(array) {
+  let answer = 0;
+  if (array == []) {
+    answer = 0;
+  }
+  else {
+    for (item of array) {
+      answer += item;
+    }
+  }
+
+  return answer;
 	
 };
 
-const sum = function() {
+const multiply = function(array) {
+  let answer = 0;
+  
+  if (array == []) {
+    answer = 0;
+  }
+  else {
+    answer += 1;
+    for (item of array) {
+      answer *= item;
+    }
+  }
+
+  return answer;
+
+};
+
+const power = function(num1, num2) {
+  return Math.pow(num1, num2)
 	
 };
 
-const multiply = function() {
+const factorial = function(num) {
+  let answer = 1;
+  if (num == 1 || num == 0) {
+    answer = 1;
+  }
+  else {
+    for (i = 1; i <= num; i++) {
+      answer *= i;
+    }
+  }
 
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+  return answer;
 	
 };
 
