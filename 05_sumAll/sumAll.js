4daa383d2e4eaa98db2c045c9244f9c20c6556a5
richtext <107862568+RchText@users.noreply.github.com>
2022-12-03 23:02:55 +0100

    Complete Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,30 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+    let sum = 0;
+    let firstNum;
+    let secondNum;
+
+    if (num1 < 0 || num2 <0 || !Number.isFinite(num1) || !Number.isFinite(num2)) {
+        sum = "ERROR";
+    }
+    else {
+        if (num1<=num2) {
+            firstNum = num1;
+            secondNum = num2;
+        }
+        else {
+            secondNum = num1;
+            firstNum = num2;
+        }
+
+        for (let i = firstNum; i <= secondNum; i++) {
+            sum += i;
+        }
+    }
+
+    
+
+    return sum;
 
 };
 
