097ef827a4565ee8957f0b427654963ea82b970d
richtext <107862568+RchText@users.noreply.github.com>
2022-12-04 02:19:43 +0100

    Complet ex.9

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fibbo = [1,1];
+    let a;
+    num = Number(num);
 
+    if (num < 0) {
+        return "OOPS";
+    }
+    else {
+        for (i=3; i<= num; i++) {
+            a = fibbo.length;
+            b = fibbo[a-1] + fibbo[a-2]
+            fibbo.push(b)
+        }
+    
+        return fibbo[num-1];
+    }
 };
 
 // Do not edit below this line
