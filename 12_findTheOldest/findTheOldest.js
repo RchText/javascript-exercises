fc741455c4f17e81d9ab5178b556ab390f16b6b3
richtext <107862568+RchText@users.noreply.github.com>
2022-12-04 12:45:58 +0100

    Complete Ex. 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    for (person of array) {
+        if (person.yearOfDeath == undefined) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+        
+        person.age = person.yearOfDeath - person.yearOfBirth;
+
+    }
+    
+    result = array.reduce((a, b) => a["age"] > b["age"] ? a : b) ;
+
+    return result
+
+    // Object.keys(obj).reduce((a, b) => obj[a] > obj[b] ? a : b);
+
 
 };
 
